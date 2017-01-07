0b6253df295ad0c6c2d2a161329a664a02f5bac9
Hamilton Clark <dhclark2@ncsu.edu>
2017-01-07 11:41:01 -0500

    Added login and logout options in navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   root "stores#index"
-  resources :stores
+  resources :stores do
+    collection do
+      get :lucky
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get    '/auth/:provider'          => 'omniauth#auth',    as: :auth
   get    '/auth/:provider/callback' => 'session#create'
   get    '/auth/failure'            => 'session#failure'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 end
