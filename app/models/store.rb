fb18bf7e64ee9a79a204949f652c392a55e43a17
Hamilton Clark <dhclark2@ncsu.edu>
2017-01-07 10:50:04 -0500

    Added price and cuisine drop downs in the create page.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,14 @@
 class Store < ApplicationRecord
+  CUISINES = ["--", "British", "American Fusion", "Bakery", "Pizza", "Italian", "Mexican", "Asian/Sushi", "Seafood", "Other"]
+  PRICES = [
+    ["--", 0],
+    ["$", 1],
+    ["$$", 2],
+    ["$$$", 3],
+    ["$$$$", 4],
+    ["$$$$$", 5]
+  ]
+
   validates :name, presence: true
   validates :address, presence: true
   validates :hours, presence: true
